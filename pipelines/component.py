2221d19ef4ec57e1dc8886f81a6f7f32e9043b63
Miguel F Ferraz <miguel.f.ferraz.27@gmail.com>
2020-02-03 15:08:00 -0300

    Add dependencies to component

--- pipelines/component.py
+++ pipelines/component.py
@@ -8,18 +8,24 @@ class Component():
         self.id = id
         self.component_name = component_name
         self.notebook_path = notebook_path
+        self.dependencies = []
+        self.container_op = None
 
-    def write_component(self):
+    def create_container_op(self):
         notebook_path = self.notebook_path
         output_path = self.notebook_path + ".out"
 
         fpath = os.path.join(os.path.dirname(__file__), 'resources',
                              'papermill.yaml')
 
-        papermill_op = components.load_component_from_file(fpath)
+        container = components.load_component_from_file(fpath)
 
-        papermill_op(notebook_path=notebook_path,
-                     output_path=output_path).set_image_pull_policy('Always')
+        self.container_op = container(
+            notebook_path=notebook_path,
+            output_path=output_path).set_image_pull_policy('Always')
+            
+    def add_dependency(self, dependency):
+        self.dependencies.append(dependency)
 
     def __str__(self):
         return '''id: {0}, component_name: {1}, notebook_path: {2}, dependencies: {3}'''.format(
--- pipelines/pipeline.py
+++ pipelines/pipeline.py
@@ -2,25 +2,75 @@ import kfp
 import kfp.compiler as compiler
 import kfp.dsl as dsl
 
+from .utils import normalize_string
+from .component import Component
 
 class Pipeline():
     def __init__(self, components):
-        self.components = components
+        self.components = self._init_components(components)
+        self._get_dependencies(components)
 
-        client = kfp.Client('0.0.0.0:31380/pipeline')
+        self.client = self._init_client()
+        
         EXPERIMENT_NAME = 'Teste'
-        experiment = client.create_experiment(name=EXPERIMENT_NAME)
+        experiment = self.client.create_experiment(name=EXPERIMENT_NAME)
 
-        self.client = client
         self.experiment = experiment
+
+    def _init_client(self):
+        return kfp.Client('0.0.0.0:31380/pipeline')
+
+    def _init_components(self, components):
+        components_objects = []
+
+        for index, component in enumerate(components):
+            try:
+                component_name = normalize_string(component['component_name'])
+                notebook_path = component['notebook_path']
+            except KeyError:
+                raise ValueError('Invalid component.')
+
+            components_objects.append(
+                Component(index, component_name, notebook_path))
+
+        return components_objects
+
+    def _search_dependency(self, dependency):
+        for component in self.components:
+            if component.component_name == dependency:
+                return component
+        raise ValueError('Invalid dependency')
+
+    def _get_dependencies(self, components):
+        for index, component in enumerate(components):
+            component_object = self.components[index]
+            try:    
+                dependencies = component['dependencies']
+                for dependency in dependencies:
+                    dependency_name = normalize_string(dependency)
+
+                    if dependency_name == component_object.component_name:
+                        raise ValueError('Self-dependent.')
+
+                    component_object.add_dependency(self._search_dependency(dependency_name))
+
+            except KeyError:
+                continue
 
     def compile_pipeline(self):
         @dsl.pipeline(name='Common pipeline')
-        def run_workflow():
-            for c in self.components:
-                c.write_component()
+        def pipeline():
+            for component in self.components:
+                component.create_container_op()
 
-        compiler.Compiler().compile(run_workflow, 'result.tar.gz')
+            for component in self.components:
+                if component.dependencies:
+                    component.container_op.after(
+                        *[dependency.container_op 
+                            for dependency in component.dependencies
+                            ])
+
+        compiler.Compiler().compile(pipeline, 'result.tar.gz')
 
     def run_pipeline(self):
         run = self.client.run_pipeline(self.experiment.id, 'result',
