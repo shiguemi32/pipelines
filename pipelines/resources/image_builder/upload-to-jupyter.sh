04c54b7b545ddd938acebb8c63e0d5aa0264a288
Miguel Ferraz <miguel.f.ferraz.27@gmail.com>
2020-05-05 10:55:04 -0300

    Update upload-to-jupyter.sh

--- pipelines/resources/image_builder/upload-to-jupyter.sh
+++ pipelines/resources/image_builder/upload-to-jupyter.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-curl -X PUT http://server.anonymous:80/notebook/anonymous/server/experiments/ -b "_xsrf: token" -H "Content-Type: application/json" -H "X-XSRFToken: token"
-curl -X PUT http://server.anonymous:80/notebook/anonymous/server/experiments/$1/ -b "_xsrf: token" -H "Content-Type: application/json" -H "X-XSRFToken: token"
-curl -X PUT http://server.anonymous:80/notebook/anonymous/server/experiments/$1/operators/ -b "_xsrf: token" -H "Content-Type: application/json" -H "X-XSRFToken: token"
-curl -X PUT http://server.anonymous:80/notebook/anonymous/server/experiments/$1/operators/$2 -b "_xsrf: token" -H "Content-Type: application/json" -H "X-XSRFToken: token"
-curl -X PUT http://server.anonymous:80/notebook/anonymous/server/experiments/$1/operators/$2/Inference.ipynb -b "_xsrf: token" -H "Content-Type: application/json" -H "X-XSRFToken: token" --data @output.ipynb
+curl -v -X PUT http://server.anonymous:80/notebook/anonymous/server/experiments/ -b "_xsrf: token" -H "Content-Type: application/json" -H "X-XSRFToken: token"
+curl -v -X PUT http://server.anonymous:80/notebook/anonymous/server/experiments/$1/ -b "_xsrf: token" -H "Content-Type: application/json" -H "X-XSRFToken: token"
+curl -v -X PUT http://server.anonymous:80/notebook/anonymous/server/experiments/$1/operators/ -b "_xsrf: token" -H "Content-Type: application/json" -H "X-XSRFToken: token"
+curl -v -X PUT http://server.anonymous:80/notebook/anonymous/server/experiments/$1/operators/$2 -b "_xsrf: token" -H "Content-Type: application/json" -H "X-XSRFToken: token"
+curl -v -X PUT http://server.anonymous:80/notebook/anonymous/server/experiments/$1/operators/$2/Inference.ipynb -b "_xsrf: token" -H "Content-Type: application/json" -H "X-XSRFToken: token" --data @output.ipynb
