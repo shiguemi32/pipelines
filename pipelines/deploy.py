a3a2582e37af539ea621d8b1bf6c2e29f2978a0e
Miguel Ferraz <miguel.f.ferraz.27@gmail.com>
2020-05-11 20:20:09 -0300

    Fix get deploys response

--- pipelines/deploy.py
+++ pipelines/deploy.py
@@ -1,13 +1,11 @@
 # -*- coding: utf-8 -*-
 import json
-
 from werkzeug.exceptions import BadRequest
 from kubernetes import client, config
 from kubernetes.client import Configuration, ApiClient
 from kubernetes.client.rest import ApiException
-
 from .pipeline import Pipeline
-from .utils import init_pipeline_client, format_pipeline_run
+from .utils import init_pipeline_client
 
 
 def deploy_pipeline(pipeline_parameters):
@@ -41,26 +39,27 @@ def get_deploys():
     Returns:
         Deploy list.
     """
+    res = {}
+
     client = init_pipeline_client()
-    runs = []
-    token = ''
-    while True:
-        list_runs = client.list_runs(
-            page_token=token, sort_by='created_at desc', page_size=100)
-        if list_runs.runs is not None:
-            for run in list_runs.runs:
-                # check if run is type deployment
-                manifest = run.pipeline_spec.workflow_manifest
-                if 'SeldonDeployment' in manifest:
-                    _run = format_pipeline_run(run)
-                    runs.append(_run)
-            token = list_runs.next_page_token
-            runs_size = len(list_runs.runs)
-            if runs_size == 0 or token is None:
-                break
-        else:
-            break
-    return {'runs': runs}
+
+    list_runs = client.list_runs(sort_by='created_at').runs
+
+    import socket
+    ip = socket.gethostbyname(socket.gethostname())
+
+    for run in list_runs:
+        manifest = run.pipeline_spec.workflow_manifest
+        if 'SeldonDeployment' in manifest:
+            res[run.name] = {
+                'status': run.status,
+                'url':
+                    'http://{}/seldon/anonymous/{}/api/v1.0/predictions'.format(
+                        ip, 'deploy-' + run.name) if run.status == 'Succeeded' else None,
+                'createdAt': run.created_at
+            }
+
+    return res
 
 
 def get_deployment_log(pod, container):
@@ -70,11 +69,11 @@ def get_deployment_log(pod, container):
         pod (str): pod name.
         container (str): container name.
     """
-    if not pod:    
+    if not pod:
         raise BadRequest('Missing the parameter: pod')
 
-    if not container:    
-        raise BadRequest('Missing the parameter: container') 
+    if not container:
+        raise BadRequest('Missing the parameter: container')
 
     config.load_incluster_config()
     v1 = client.CoreV1Api()
@@ -94,4 +93,3 @@ def get_deployment_log(pod, container):
         body = json.loads(e.body)
         error_message = body['message']
         raise BadRequest('{}'.format(error_message))
-    
--- pipelines/utils.py
+++ pipelines/utils.py
@@ -3,15 +3,14 @@ import json
 import re
 
 from os import getenv
-from unicodedata import normalize
 
-from kfp import compiler, dsl, Client
-from schema import Schema, SchemaError, Use, Or, Optional
+from kfp import Client
+from schema import Schema, SchemaError, Or, Optional
 from werkzeug.exceptions import BadRequest
 
 
 def init_pipeline_client():
-    """Create a new kfp client. 
+    """Create a new kfp client.
 
     Returns:
         An instance of kfp client.
@@ -60,84 +59,15 @@ def validate_notebook_path(notebook_path):
         raise BadRequest('Invalid notebook path. ' + notebook_path)
 
 
-def format_pipeline_run(run):
-    # format run response
-    resp_run = {}
-    resp_run['id'] = run.id
-    resp_run['name'] = run.name
-    resp_run['createdAt'] = run.created_at
-    resp_run['finishedAt'] = run.finished_at
-    resp_run['description'] = run.description
-    resp_run['error'] = run.error
-    resp_run['status'] = run.status
-    resp_run['scheduledAt'] = run.scheduled_at
-    resp_run['storageState'] = run.storage_state
-
-    # format run pipeline spec response
-    pipeline_spec = run.pipeline_spec
-    resp_pipeline_spec = {}
-    if pipeline_spec is not None:
-        resp_pipeline_spec['pipelineId'] = pipeline_spec.pipeline_id
-        resp_pipeline_spec['pipelineManifest'] = pipeline_spec.pipeline_manifest
-        resp_pipeline_spec['pipelineName'] = pipeline_spec.pipeline_name
-        resp_pipeline_spec['workflowManifest'] = json.loads(
-            pipeline_spec.workflow_manifest)
-        parameters = []
-        if pipeline_spec.parameters is not None:
-            for parameter in pipeline_spec.parameters:
-                _parameter = {
-                    'name':  parameter.name,
-                    'value': parameter.value
-                }
-                parameters.append(_parameter)
-        resp_pipeline_spec['parameters'] = parameters
-    resp_run['pipelineSpec'] = resp_pipeline_spec
-
-    # format run metrics response
-    metrics = []
-    if run.metrics is not None:
-        for metric in run.metrics:
-            _metric = {
-                'format':  metric.format,
-                'name': metric.name,
-                'nodeId': metric.node_id,
-                'numberValue': metric.number_value
-            }
-            metrics.append(_metric)
-    resp_run['metrics'] = metrics
-
-    # format run resource references response
-    resource_references = []
-    if run.resource_references is not None:
-        for references in run.resource_references:
-            _reference = {
-                'name': references.name,
-                'relationship': references.relationship,
-                'key': {
-                    'id': references.key.id,
-                    'type': references.key.type
-                }
-            }
-            resource_references.append(_reference)
-    resp_run['resourceReferences'] = resource_references
-
-    return resp_run
-
-
 def format_pipeline_run_details(run_details):
-    run = run_details.run
-
     workflow_manifest = json.loads(
         run_details.pipeline_runtime.workflow_manifest)
     nodes = workflow_manifest['status']['nodes']
 
-    pipeline_status = ''
     components_status = {}
 
     for index, component in enumerate(nodes.values()):
-        if index == 0:
-            pipeline_status = component['phase']
-        else:
+        if index != 0:
             components_status[str(component['displayName'])[
                 7:]] = str(component['phase'])
 
